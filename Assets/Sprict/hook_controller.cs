78cc0487b3185a823a2552df33f1236ad6bd06bc
JDForest460 <46009704+JDForest460@users.noreply.github.com>
2021-01-27 00:30:33 -0500

    add pull hook

---
+++ Assets/Sprict/hook_controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hook_controller : MonoBehaviour
+{
+
+    public Rigidbody2D hook_rb;
+    public float hook_power;
+    public LineRenderer fish_line;
+    private bool is_pull_hook;
+    void Start()
+    {
+        fish_line.SetPosition(0, hook_rb.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        draw_line();
+        pull_hook();
+
+    }
+    void draw_line()
+	{
+        fish_line.SetPosition(1, hook_rb.transform.position);
+	}
+    void pull_hook()
+	{
+        is_pull_hook = Input.GetMouseButton(0);
+        if (is_pull_hook)
+		{
+            hook_rb.velocity = new Vector2(hook_rb.velocity.x, hook_power * Time.deltaTime);
+        }
+	}
+}
